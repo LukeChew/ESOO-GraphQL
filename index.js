88b133eb3ed0756a0e5aef9fe75a0d05c6f678a0
Luke Chew <63369645+LukeChew@users.noreply.github.com>
2021-06-29 11:51:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const typeDefs = require('./typeDefs');
 
 const resolvers = require('./resolvers');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, playground: true,
+  introspection: true });
 server
     .listen(PORT)
     .then(({ url }) => console.log(`🚀 Server running at ${url}`));
